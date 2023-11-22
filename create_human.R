eba0eb882c5d47ec38e254a3e7932a82b8c99f2f
Konrad Sopyllo <konradsopyllo@hotmail.com>
2023-11-22 16:02:19 +0200

    Assignment 4

---
+++ create_human.R
@@ -0,0 +1,73 @@
+
+
+
+library(readr)
+hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
+gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
+
+#Human development dataset
+str(hd)
+dim(hd)
+summary(hd)
+colnames(hd)
+
+# Gender inequality dataset
+str(gii)
+dim(gii)
+summary(gii)
+colnames(gii)
+
+#Renaming into more descriptive names
+
+library(dplyr)
+
+# Renaming the "Human development" 
+library(dplyr)
+
+# Rename variables in the "Human development" dataset
+hd <- hd %>%
+  rename(
+    HDI_Rank = `HDI_Rank`,
+    Country = `Country`,
+    HDI = `Human_Development_Index`,
+    Life.Exp = `Life_Expectancy_at_Birth`,
+    Edu.Exp = `Expected_Years_of_Education`,
+    Mean.Edu = `Mean_Years_of_Education`,
+    GNI.percapita = `Gross_National_Income_per_Capita`,
+    GNI_HDI = `GNI_per_Capita_Rank_Minus_HDI_Rank`
+  )
+
+# Rename variables in the "Gender inequality" dataset
+gii <- gii %>%
+  rename(
+    Country = `Country`,
+    GII = `Gender Inequality Index (GII)`,
+    Mat.Mor = `Maternal Mortality Ratio`,
+    Ado.Birth = `Adolescent Birth Rate`,
+    Parli.F = `Percent Representation in Parliament`,
+    Edu2.F = `Population with Secondary Education (Female)`,
+    Edu2.M = `Population with Secondary Education (Male)`,
+    Labo.F = `Labour Force Participation Rate (Female)`,
+    Labo.M = `Labour Force Participation Rate (Male)`,
+  )
+
+head(hd)
+head(gii)
+
+#Adding two new variables. Edu2.FM = Edu2.F / Edu2.M, Labo.FM = Labo.F / Labo.M
+
+gender_data <- gii %>%
+  mutate(
+    Edu2.FM = Edu2.F / Edu2.M,
+    Labo.FM = Labo.F / Labo.M
+  )
+
+head(gender_data)
+
+#Joining the data together
+
+human <- inner_join(hd, gii, by = "Country")
+
+head(human)
+#saving the data
+write_csv(human, "data/human.csv")
